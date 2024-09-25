543abe9e99d6c07ef71b85dc01faeda3e03dec85
Yuriy <yura-maksuta@mail.ru>
2024-09-25 12:27:56 +0300

    Improved prompt

--- processing.py
+++ processing.py
@@ -35,11 +35,11 @@ class DrugInteractionProcessor:
             chat_completion = openai.chat.completions.create(
                 model="meta-llama/Meta-Llama-3.1-405B-Instruct",
                 messages=[{"role": "user", "content": f'''Summarize the following text in {language}, use bullet points, start answering with summary,
-                            write only important information about interactions. Split information by 5 parts: summary result, danger interaction, medium-risk interaction,
+                            write only important information about interactions. Output should be strictly formatted by 5 parts: summary result, danger interaction, medium-risk interaction,
                             low-risk, no-risk interaction. First section is about short one-sentence summary of following information like (write to 
                            the end of this sentence color in braces like red, yellow and green which represents treat level -- USE ONE WORD WITH symbols like !yellow!): ok treatment or dangerous
                             treatment. Don't write about consulting doctors, we will push user to this manually. Also write paragraph named
-                            duplication shortly. Use $ symbol as separator between every section it's very important to use $ symbol as separator!:\n{text}'''}],
+                            duplication shortly. Use $ symbol as separator between every section it's very important to use $ symbol as separator -- ITS REALLY STRICT REQUIREMENT FOR PARSING! After every section name use symbol : it's strictly for parsing!!!:\n{text}'''}],
             )
             return chat_completion.choices[0].message.content
         
